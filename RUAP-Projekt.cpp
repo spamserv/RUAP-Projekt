d649eec7670ce5acc285ae38d47005515d0c1da0
Josip Vojak <josipvojak@gmail.com>
2016-04-12 13:01:20 +0200

    Added Hello World message

---
+++ RUAP-Projekt.cpp
@@ -0,0 +1,13 @@
+// RUAP-Projekt.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Pozdrav iz aplikacije od jvojak");
+	getchar();
+	return 0;
+}
+
